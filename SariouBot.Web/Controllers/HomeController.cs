dd17ce65c975fce6ae061f57e5e793823432722e
varcyon <darkenedmagi@gmail.com>
2022-09-01 18:53:42 -0500

    getting started with webstuff

--- SariouBot.Web/Controllers/HomeController.cs
+++ SariouBot.Web/Controllers/HomeController.cs
@@ -4,29 +4,13 @@ using System.Diagnostics;
 
 namespace SariouBot.Web.Controllers
 {
+    [Route("")]
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public IActionResult Index()
         {
-            return View();
+            return Ok("Hello!");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
